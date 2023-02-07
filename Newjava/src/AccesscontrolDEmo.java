94f2fa6c023e34385e81e788930d882b1f44ac5c
landge sonali <landgesonali1998@gmail.com>
2023-02-07 23:06:24 +0530

    check the access control

---
+++ Newjava/src/AccesscontrolDEmo.java
@@ -0,0 +1,18 @@
+
+public class AccesscontrolDEmo {
+ int a; //default access
+ public int b; //public access
+ private int c; //private access
+public int setc;
+ void setc(int i)
+ {
+	c=i;
+
+ }
+ int getc() 
+	{
+	 return c;
+ }
+
+ 
+}
