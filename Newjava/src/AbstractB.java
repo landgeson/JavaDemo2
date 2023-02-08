f3b28f02837b7ef8ed3733a0629c761f038d7412
landge sonali <landgesonali1998@gmail.com>
2023-02-08 15:25:26 +0530

    demo on abstract

---
+++ Newjava/src/AbstractB.java
@@ -0,0 +1,9 @@
+
+public class AbstractB extends AbstractA {
+
+	void callme()
+	{
+		System.out.println("Implemetation of call me which is an abstract method");
+	}
+
+}
---
+++ Newjava/src/AbstractDemo.java
@@ -0,0 +1,12 @@
+
+public class AbstractDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AbstractB obj= new AbstractB();
+		
+		obj.callme();
+		obj.callmetoo();
+	}
+
+}
